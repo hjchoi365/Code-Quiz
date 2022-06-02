8e9be7cfeec2dfac581e423cb60e9c59090f7a6c
Hail Choi <julianchoiii11@gmail.com>
2022-06-02 14:51:09 -0700

    Worked on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,4 +41,92 @@ var questArray = [
     }
     ]
 
-    
+var scores = [];
+
+var body = document.body;
+var mainpageEl = document.getElementById("main-page");
+var startquizEl = document.getElementById("start-quiz");
+var questformEl = document.getElementById("quest-form");
+var questionsEl = document.getElementById("questions");
+var questionEl = document.getElementById("question");
+var choice1El = document.getElementById("choice1");
+var choice2El = document.getElementById("choice2");
+var choice3El = document.getElementById("choice3");
+var choice4El = document.getElementById("choice4");
+var resultEl = document.getElementById("result");
+var verifyEl = document.getElementById("verify");
+var subFormEl = document.getElementById("submit-form");
+var inputEl = document.getElementById("initial")
+var viewscoreEl = document.getElementById("viewscore");
+var headerEl = document.getElementById("head");
+var loadHighscoresEl = document.createElement("div");
+var gobackButtonEl = document.createElement("button");
+var clearButtonEl = document.createElement("button");
+var olistItemEl;
+var clearButtonel;
+var correctAns="";
+
+var cnt = 0;
+
+var d = 75;
+var myVar = "";
+var Hgihscore = 0;
+
+var myTimer = function() {
+    d=d-1;
+    document.getElementById("timer").innerHTML = d;
+    if (d<0 || d === 0) {
+        clearInterval(myVar);
+    }
+};
+
+var showResult = function() {
+    clearInterval(myVar);
+
+    if (d<0) {
+        document.getElementById("timer").innerHTML = 9;
+        Highscore = 0;
+    }
+    else {
+        document.getElementById("timer").innerHTML = d;
+        Highscore = d;
+    }
+    localStorage.setItem("hScore", Highscore);
+    questionsEl.parentNode.removeChild(questionsEl);
+    body.appendChild(subFormEl);
+    body.appendChild(resultEl);
+    document.getElementById("fscore").textContent = Highscore;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
