d8256e70321e761ae52d87c9319e061631c47b1c
Hail Choi <julianchoiii11@gmail.com>
2022-06-02 13:35:55 -0700

    Worked on css, started on js

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+var questArray = [
+    {
+        "questionText": "Commonly used data types that do not include:_____________",
+        "opt1" : "1. Booleans",
+        "opt2" : "2. Alerts",
+        "opt3" : "3. Strings",
+        "opt4" : "4. Numbers",
+        "correct" : "2. Alerts"
+    },
+    {
+        "questionText": "The condition in an if / else statement is enclosed with __________",
+        "opt1" : "1. Paranthesis",
+        "opt2" : "2. Sqaure brackets",
+        "opt3" : "3. Quotes",
+        "opt4" : "4. Curly brackets",
+        "correct" : "1. Paranthesis"
+    },
+    {
+        "questionText": "Arrays in Javascript can be used to store _____________",
+        "opt1" : "1. other arrays",
+        "opt2" : "2. Booleans",
+        "opt3" : "3. Numbers and Strings",
+        "opt4" : "4. All of the above",
+        "correct" : "4. All of the above"
+    },
+    {
+        "questionText": "String values must be enclosed within _____ when being assigned to variable",
+        "opt1" : "1. Quotes",
+        "opt2" : "2. Commas",
+        "opt3" : "3. Parenthesis",
+        "opt4" : "4. Curly brackets",
+        "correct" : "1. Quotes"
+    },
+    {
+        "questionText": "A useful tool used during development & debugging for printing content to the debugger is",
+        "opt1" : "1. For loops",
+        "opt2" : "2. Console.log",
+        "opt3" : "3. Terminal/bash",
+        "opt4" : "4. Javascript",
+        "correct" : "4. Console.log"
+    }
+    ]
+
+    
