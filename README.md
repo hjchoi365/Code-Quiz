a58a0b9d7752b37baf718a7838f66732e4bdbf45
Hail Choi <julianchoiii11@gmail.com>
2022-06-05 17:39:22 -0700

    Added README file

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Code-Quiz
+
+## Purpose:
+The purpose for this application is to build a timed coding quiz that will punish incorrect answers. The application will run in the browser and will feature HTML and CSS, powered by JavaScript that is written. 
+
+
+## Description:
+When the "Start Quiz" button is clicked, the user will be given 75 seconds to start. 
+There is a total of 5 questions, additionally there is a penalty of -10 seconds for each incorrect answer. 
+When all the questions are answered or timer reaches to zero then the game is over.While last couple of seconds remaining,if user answers last question with wrong answer, then the final score will be zero and not negative. 
+When the game is over, user can save initials and score.
+On click of view high score link user will be shown list of all the high scores. On click of clear high scores, all the saved scores will be removed.
+On click of Go back button, user will be directed to main page and can start taking a code quiz.
+
+
+## User Story
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+## Acceptance crieteria:
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+
+## Mock-Up
+The following animation demonstrates the application functionality:
+
+<div>
+    <img src="./assets/images/coding-quiz-pic" width="400px"/> 
+</div>
+
+## Built With
+* HTML
+* CSS
+* JavaScript
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,54 @@
+//defines questions array
 var questArray = [
     {
         "questionText": "Commonly used data types that do not include:_____________",
-        "opt1": "1. Booleans",
-        "opt2": "2. Alerts",
-        "opt3": "3. Strings",
-        "opt4": "4. Numbers",
-        "correct": "2. Alerts"
+        "cht1": "1. Strings",
+        "cht2": "2. Booleans",
+        "cht3": "3. Alerts",
+        "cht4": "4. Numbers",
+        "correct": "3. Alerts"
     },
     {
         "questionText": "The condition in an if / else statement is enclosed with __________",
-        "opt1": "1. Paranthesis",
-        "opt2": "2. Sqaure brackets",
-        "opt3": "3. Quotes",
-        "opt4": "4. Curly brackets",
-        "correct": "1. Paranthesis"
+        "cht1": "1. Quotes",
+        "cht2": "2. Curly brackets",
+        "cht3": "3. Paranthesis",
+        "cht4": "4. Square brackets",
+        "correct": "3. Paranthesis"
     },
     {
         "questionText": "Arrays in Javascript can be used to store _____________",
-        "opt1": "1. other arrays",
-        "opt2": "2. Booleans",
-        "opt3": "3. Numbers and Strings",
-        "opt4": "4. All of the above",
+        "cht1": "1. Numbers and Strings",
+        "cht2": "2. Other arrays",
+        "cht3": "3. Booleans",
+        "cht4": "4. All of the above",
         "correct": "4. All of the above"
     },
     {
         "questionText": "String values must be enclosed within _____ when being assigned to variable",
-        "opt1": "1. Quotes",
-        "opt2": "2. Commas",
-        "opt3": "3. Parenthesis",
-        "opt4": "4. Curly brackets",
-        "correct": "1. Quotes"
+        "cht1": "1. Commas",
+        "cht2": "2. Curly brackets",
+        "cht3": "3. Quotes",
+        "cht4": "4. Parenthesis",
+        "correct": "3. Quotes"
     },
     {
         "questionText": "A useful tool used during development & debugging for printing content to the debugger is",
-        "opt1": "1. For loops",
-        "opt2": "2. Console.log",
-        "opt3": "3. Terminal/bash",
-        "opt4": "4. Javascript",
+        "cht1": "1. Javascript",
+        "cht2": "2. Terminal/bash",
+        "cht3": "3. For loops",
+        "cht4": "4. Console.log",
         "correct": "4. Console.log"
     }
 ]
-
+//define scores empty array 
 var scores = [];
-
+//identify all the elements in application
 var body = document.body;
-var mainpageEl = document.getElementById("main-page");
+var mainpageEl = document.getElementById("main-page")
 var startquizEl = document.getElementById("start-quiz");
 var questformEl = document.getElementById("quest-form");
-var questionsEl = document.getElementById("questions");
+var questionsEl = document.getElementById("questions")
 var questionEl = document.getElementById("question");
 var choice1El = document.getElementById("choice1");
 var choice2El = document.getElementById("choice2");
@@ -56,7 +57,7 @@ var choice4El = document.getElementById("choice4");
 var resultEl = document.getElementById("result");
 var verifyEl = document.getElementById("verify");
 var subFormEl = document.getElementById("submit-form");
-var inputEl = document.getElementById("initial")
+var inputEl = document.getElementById("initial");
 var viewscoreEl = document.getElementById("viewscore");
 var headerEl = document.getElementById("head");
 var loadHighscoresEl = document.createElement("div");
@@ -65,13 +66,14 @@ var clearButtonEl = document.createElement("button");
 var olistItemEl;
 var clearButtonEl;
 var correctAns = "";
-
+//counter to keep track of questions
 var cnt = 0;
-
+//counter to keep track of time 
 var d = 75;
 var myVar = "";
 var Highscore = 0;
 
+//function to keep track of time
 var myTimer = function () {
     d = d - 1;
     document.getElementById("timer").innerHTML = d;
@@ -80,11 +82,12 @@ var myTimer = function () {
     }
 };
 
+//function to show results once questions over
 var showResult = function () {
     clearInterval(myVar);
 
     if (d < 0) {
-        document.getElementById("timer").innerHTML = 9;
+        document.getElementById("timer").innerHTML = 0;
         Highscore = 0;
     }
     else {
@@ -95,9 +98,10 @@ var showResult = function () {
     questionsEl.parentNode.removeChild(questionsEl);
     body.appendChild(subFormEl);
     body.appendChild(resultEl);
-    document.getElementById("final-score").textContent = Highscore;
+    document.getElementById("fscore").textContent = Highscore;
 };
 
+//capture event when answers selected
 var buttonclick = function (event) {
     var Seletedans = document.getElementById(this.id).innerText;
     if ((cnt < questArray.length && d > 0) || (cnt < questArray.length && d === 0)) {
@@ -125,15 +129,17 @@ var buttonclick = function (event) {
     }
 };
 
+//load questions 
 var loadQuestions = function () {
     questionEl.textContent = questArray[cnt].questionText;
-    choice1El.textContent = questArray[cnt].opt1;
-    choice2El.textContent = questArray[cnt].opt2;
-    choice3El.textContent = questArray[cnt].opt3;
-    choice4El.textContent = questArray[cnt].opt4;
+    choice1El.textContent = questArray[cnt].cht1;
+    choice2El.textContent = questArray[cnt].cht2;
+    choice3El.textContent = questArray[cnt].cht3;
+    choice4El.textContent = questArray[cnt].cht4;
     correctAns = questArray[cnt].correct;
 };
 
+//Start quiz button clicked
 var startQuiz = function (event) {
     document.getElementById("timer").innerHTML = 75;
     myVar = setInterval(myTimer, 1000);
@@ -143,14 +149,15 @@ var startQuiz = function (event) {
     body.appendChild(resultEl);
 };
 
-var inputText = function(event) {
+//remove the results section on keyup event
+var inputText = function (event) {
     verifyEl.innerText = "";
     if (document.body.contains(resultEl)) {
         resultEl.parentNode.removeChild(resultEl);
     }
 };
 
-
+//load scores from local storage
 var loadScores = function () {
     var getScores = localStorage.getItem("scores");
 
@@ -163,7 +170,7 @@ var loadScores = function () {
 
 };
 
-
+//submit form event with scores and initial
 var submitForm = function (event) {
     event.preventDefault();
     loadScores();
@@ -192,7 +199,7 @@ var submitForm = function (event) {
     loadHighscores(true);
 };
 
-
+//view high scores
 var loadHighscores = function (flag) {
     var getScores = localStorage.getItem("scores");
     if (flag === true) {
@@ -204,6 +211,7 @@ var loadHighscores = function (flag) {
         olistItemEl.setAttribute("id", "ollist");
         loadHighscoresEl.appendChild(olistItemEl);
 
+        //create a li element and show scores from local storage
         if (getScores !== null) {
 
             getScores = JSON.parse(getScores);
@@ -215,13 +223,13 @@ var loadHighscores = function (flag) {
                 olistItemEl.appendChild(listItemEl);
             }
         }
-
+        // create Go Back button
         gobackButtonEl.textContent = "Go Back";
-        gobackButtonEl.className = "btn hscores-gobtn";
+        gobackButtonEl.className = "btn1 hscores-gobtn";
 
-  
+        //create clear high scores button
         clearButtonEl.textContent = "Clear high scores";
-        clearButtonEl.className = "btn hscores-clbtn";
+        clearButtonEl.className = "btn1 hscores-clbtn";
 
         if (getScores === null) {
             clearButtonEl.disabled = true;
@@ -241,7 +249,7 @@ var loadHighscores = function (flag) {
     }
 
 };
-
+//On click of View High score link
 var viewhscores = function (event) {
     if (document.body.contains(questionsEl)) {
         correctAns = "";
@@ -274,13 +282,14 @@ var viewhscores = function (event) {
     loadHighscores(true);
 };
 
+//go back button cliked
 var goback = function (event) {
     body.removeChild(loadHighscoresEl);
     body.appendChild(headerEl);
     body.appendChild(mainpageEl);
 };
 
-
+//clear high scores button clicked
 var clearHighsores = function (event) {
 
     localStorage.clear();
@@ -290,7 +299,7 @@ var clearHighsores = function (event) {
     loadHighscores(false);
 };
 
-
+//event handlers
 questionsEl.parentNode.removeChild(questionsEl);
 resultEl.parentNode.removeChild(resultEl);
 subFormEl.parentNode.removeChild(subFormEl);
@@ -304,7 +313,3 @@ subFormEl.addEventListener("submit", submitForm);
 viewscoreEl.addEventListener("click", viewhscores);
 gobackButtonEl.addEventListener("click", goback);
 clearButtonEl.addEventListener("click", clearHighsores);
-
-
-
-
